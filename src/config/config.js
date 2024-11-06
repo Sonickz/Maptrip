ac28704d87e265c9013dac2f829cbe4efcc85021
Giovanny Ladino <ladigiococ@gmail.com>
2024-11-06 01:48:26 -0500

    Env errors correction

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,2 @@
-export const DATABASE_URL = process.env.DATABASE_URL
-export const API_URL = process.env.API_URL || "http://localhost:3000/api"
-export const WEB_NAME = process.env.WEB_NAME || "Maptrip"
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
+export const WEB_NAME = process.env.NEXT_PUBLIC_WEB_NAME

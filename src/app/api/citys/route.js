d273df0bdc6d7dfa05486e55d873d4b0a9e03e40
Giovanny Ladino <ladigiococ@gmail.com>
2024-11-05 23:31:49 -0500

    Edit citys api

--- src/app/api/citys/route.js
+++ src/app/api/citys/route.js
@@ -14,32 +14,15 @@ export async function GET() {
 }
 
 export async function POST(req) {
-    const body = await req.json()
+    const data = await req.json()
 
     try {
-        if (!body.code) {
-            const newCitys = await prisma.citys.createMany({
-                data: body
-            })
-            return NextResponse.json({
-                message: "Citys created",
-                data: newCitys
-            }, { status: 200 })
-        }
-
-        const { code, name, description, history } = body
-        const newCity = await prisma.citys.create({
-            data: {
-                code,
-                name,
-                description,
-                history
-            }
+        const newCitys = await prisma.citys.createMany({
+            data: data
         })
-
         return NextResponse.json({
-            message: "City created",
-            data: newCity
+            message: "Citys created",
+            data: newCitys
         }, { status: 200 })
     }
     catch (error) {

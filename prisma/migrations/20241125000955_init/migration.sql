fda7582ddf4c673475ed837a721c50d13518db33
Giovanny Ladino <ladigiococ@gmail.com>
2024-11-24 22:12:06 -0500

    delete prisma migrations

--- prisma/migrations/20241125000955_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[preferenceId]` on the table `Travels` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Travels_preferenceId_key" ON "Travels"("preferenceId");
